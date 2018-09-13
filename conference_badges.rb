926c2348f20510498af656f622cd4761adeb4477
Casey Zimmer <czimmer@ramapo.edu>
2018-09-13 20:27:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ end
 
 
 def batch_badge_creator(array)
-  output_Array = []
-  array.each {|element| outputArray.push(badge_maker(name))}
-  return outputArray
+  output_array = []
+  array.each {|element| output_array.push(badge_maker(name))}
+  return output_array
 end
